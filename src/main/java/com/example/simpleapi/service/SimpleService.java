8edf63801a747b4630625fe6bf6e6102ee443aa7
남궁성환 <saturn203@naver.com>
2021-12-31 14:34:01 +0900

    mutil

--- src/main/java/com/example/simpleapi/service/SimpleService.java
+++ src/main/java/com/example/simpleapi/service/SimpleService.java
@@ -1,12 +1,13 @@
 package com.example.simpleapi.service;
 
-import com.example.simple.model.HelloReply;
-import com.example.simple.model.HelloRequest;
-import com.example.simple.model.SimpleApiGrpc.SimpleApiImplBase;
-import com.example.simple.model.SimpleReply;
-import com.example.simple.model.SimpleRequest;
-import com.example.simple.model.VersionReply;
-import com.example.simple.model.VersionRequest;
+ 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.simple.proto.HelloRequest;
+import com.example.simple.proto.HelloResponse;
+import com.example.simple.proto.HelloServiceGrpc.HelloServiceImplBase;
 
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
@@ -14,35 +15,86 @@ import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
 @Slf4j
-public class SimpleService extends SimpleApiImplBase {
+public class SimpleService extends HelloServiceImplBase {
     
-    @Override
-    public void hello(HelloRequest req, StreamObserver<HelloReply> resObserver){
-        HelloReply helloReply = HelloReply.newBuilder().setHello("Hello world").build();
-        resObserver.onNext(helloReply);
-        resObserver.onCompleted();
+    // unary
+  @Override
+  public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+
+    String greeting = request.getFirstName() + "," + request.getLastName();
+
+    HelloResponse response = HelloResponse.newBuilder()
+        .setGreeting(greeting)
+        .build();
+
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+
+  // server stream
+  @Override
+  public void helloServerStream(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+
+    List<String> greetingList = new ArrayList<>();
+    for (int i = 1; i <= 3; i++) {
+      greetingList.add(request.getFirstName() + "," + request.getLastName() + ":" + i);
     }
 
-    @Override
-    public void version(VersionRequest req, StreamObserver<VersionReply> resObserver){
-        VersionReply versionReply = VersionReply.newBuilder().setVersion("v-1.0").build();
-        resObserver.onNext(versionReply);
-        resObserver.onCompleted();
+    for (String greeting : greetingList) {
+      HelloResponse response = HelloResponse.newBuilder()
+          .setGreeting(greeting)
+          .build();
+      responseObserver.onNext(response);
     }
 
-    @Override
-    public void simple(SimpleRequest req, StreamObserver<SimpleReply> resObserver){
-        log.info("=====simple");
-        for(int i=0; i< 10 ;i++){            
-            resObserver.onNext(
-                SimpleReply.newBuilder()
-                    .setNum(i+1)
-                    .setTitle("title-"+(i+1))
-                    .setContent("content-"+(i+1))
-                    .build()
-            );            
+    responseObserver.onCompleted();
+  }
+
+  // client stream
+  @Override
+  public StreamObserver<HelloRequest> helloClientStream( StreamObserver<HelloResponse> responseObserver) {
+    return new StreamObserver<HelloRequest>() {
+        @Override
+        public void onNext(HelloRequest helloRequest) {
+            System.out.println(helloRequest.getFirstName() + "," + helloRequest.getLastName());
+            // 주의 : 여기서 responseObserver.onNext를 쓰면 biStream이 되버림
         }
-        resObserver.onCompleted();
-        log.info("=====simple end");
-    }
+
+        @Override
+        public void onError(Throwable throwable) {
+            System.out.println("error");
+        }
+
+        @Override
+        public void onCompleted() {
+            responseObserver.onNext(HelloResponse.newBuilder().setGreeting("success").build());
+            responseObserver.onCompleted();
+        }
+    };
+  }
+
+  // bi stream
+  @Override
+  public StreamObserver<HelloRequest> helloBiStream( StreamObserver<HelloResponse> responseObserver) {
+    return new StreamObserver<HelloRequest>() {
+        @Override
+        public void onNext(HelloRequest helloRequest) {
+            String greeting = helloRequest.getFirstName() + "," + helloRequest.getLastName();
+            System.out.println(greeting);
+
+            responseObserver.onNext(HelloResponse.newBuilder().setGreeting(greeting+"1").build());
+            responseObserver.onNext(HelloResponse.newBuilder().setGreeting(greeting+"2").build());
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            System.out.println("error");
+        }
+
+        @Override
+        public void onCompleted() {
+            responseObserver.onCompleted();
+        }
+    };
+  }
 }
