f3143b9b3c9d3b9abfd7359d9837ed68d0a4d1f3
남궁성환 <saturn203@naver.com>
2022-01-03 00:01:12 +0900

    dd

--- src/main/java/com/example/simpleapi/model/Simple.java
+++ src/main/java/com/example/simpleapi/model/Simple.java
@@ -1,6 +1,7 @@
 package com.example.simpleapi.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class Simple {
 	
 	private int num;
