7baf6a1d52662a2c6e470478d190f26cd8f9d50e
남궁성환 <saturn203@naver.com>
2022-08-06 19:50:51 +0900

    dfsfsd

--- src/main/java/com/example/simpleapi/SimpleApiApplication.java
+++ src/main/java/com/example/simpleapi/SimpleApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleApiApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApiApplication.class, args);
 	}
