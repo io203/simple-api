bfcd4593473fcf8b3c2dee46a5625a17da755261
남궁성환 <saturn203@naver.com>
2023-02-13 18:40:20 +0900

    아직 미완성

--- src/main/java/com/example/simpleapi/controller/SimpleApiController.java
+++ src/main/java/com/example/simpleapi/controller/SimpleApiController.java
@@ -2,29 +2,30 @@ package com.example.simpleapi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.simpleapi.model.Simple;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.simpleapi.model.Simple;
+
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class SimpleApiController {
 	@GetMapping("/hello")
-	public String hello() {
+	public Mono<String> hello() {
 		
 		log.info("==========simple-api home()");
 		
-		return "hello world";
+		return Mono.just("hello world");
 		
 	}
 	@GetMapping("/simple")
-	public List<Simple> listSimple(){
+	public Flux<List<Simple>> listSimple(){
 		List<Simple> list = new ArrayList<>();
 		
 		for(int i=0 ; i< 10;i++) {
@@ -32,15 +33,17 @@ public class SimpleApiController {
 			log.info("for "+i);
 		}
 		log.info(list.toString());
-		return list;
+		return Flux.just(list);
 		
 	}
 	
 	@GetMapping("/version")
-	public String version(){
-		//log.info("version 1.0");
-		return "=====  version 1.0";
+	public Mono<String> version(){
+		log.info("version 1.0");
+		
+		return Mono.just("version 1.0");
 		
 	}
-
+	
 }
+
