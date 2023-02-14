dcc81891a5112791c6956e7bbbc5b2f8f9fa6374
남궁성환 <saturn203@naver.com>
2023-02-14 12:28:51 +0900

    아직 미완성

--- src/main/java/com/example/simpleapi/controller/SimpleApiController.java
+++ src/main/java/com/example/simpleapi/controller/SimpleApiController.java
@@ -9,39 +9,37 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.simpleapi.model.Simple;
 
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class SimpleApiController {
 	@GetMapping("/hello")
-	public Mono<String> hello() {
+	public String hello() {
 		
 		log.info("==========simple-api home()");
 		
-		return Mono.just("hello world");
+		return "hello world";
 		
 	}
 	@GetMapping("/simple")
-	public Flux<List<Simple>> listSimple(){
+	public List<Simple> listSimple(){
 		List<Simple> list = new ArrayList<>();
 		
 		for(int i=0 ; i< 10;i++) {
 			list.add(new Simple(i+1,"test-"+i, "contents-"+i));
-			log.info("for "+i);
+			
 		}
 		log.info(list.toString());
-		return Flux.just(list);
+		return list;
 		
 	}
 	
 	@GetMapping("/version")
-	public Mono<String> version(){
+	public String version(){
 		log.info("version 1.0");
 		
-		return Mono.just("version 1.0");
+		return "version 1.0";
 		
 	}
 	
