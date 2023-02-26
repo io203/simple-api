fd4db339bb31ad374a97d73f06d36d4eaba82440
남궁성환 <saturn203@naver.com>
2023-02-26 16:09:01 +0900

    성공

--- src/main/java/com/example/simpleapi/SimpleApiApplication.java
+++ src/main/java/com/example/simpleapi/SimpleApiApplication.java
@@ -2,9 +2,6 @@ package com.example.simpleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.micrometer.observation.ObservationTextPublisher;
 
 @SpringBootApplication
 public class SimpleApiApplication {	
