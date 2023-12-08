220a10673a66dab0195619695e7201e2ac586cf2
남궁성환 <saturn203@naver.com>
2023-12-08 10:09:23 +0900

    성공

--- src/main/java/com/example/simpleapi/controller/SimpleApiController.java
+++ src/main/java/com/example/simpleapi/controller/SimpleApiController.java
@@ -1,13 +1,18 @@
 package com.example.simpleapi.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.simpleapi.model.Simple;
+import javax.naming.NamingException;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.simpleapi.model.Simple;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,30 +22,42 @@ import lombok.extern.slf4j.Slf4j;
 public class SimpleApiController {
 	@GetMapping("/hello")
 	public String hello() {
-		
+
 		log.info("==========simple-api home()");
-		
+
 		return "hello world";
-		
+
 	}
+
 	@GetMapping("/simple")
-	public List<Simple> listSimple(){
+	public List<Simple> listSimple() {
 		List<Simple> list = new ArrayList<>();
-		
-		for(int i=0 ; i< 10;i++) {
-			list.add(new Simple(i+1,"test-"+i, "contents-"+i));
-			log.info("for "+i);
+
+		for (int i = 0; i < 10; i++) {
+			list.add(new Simple(i + 1, "test-" + i, "contents-" + i));
+			log.info("for " + i);
 		}
 		log.info(list.toString());
 		return list;
-		
+
 	}
-	
+
 	@GetMapping("/version")
-	public String version(){
-		//log.info("version 1.0");
+	public String version() {
+		// log.info("version 1.0");
 		return "=====simple-api  version 1.0";
+
+	}
+
+	@GetMapping("/nslookup")
+	public void getIpByHostName() throws UnknownHostException, NamingException {
+		String hostname = "headless-simple-svc.api.svc.cluster.local";
 		
+		InetAddress[] iaArray = InetAddress.getAllByName(hostname);
+		for (InetAddress ia : iaArray) {
+			log.info("=== ip : {}",ia.getHostAddress());
+
+		}
 	}
 
 }
