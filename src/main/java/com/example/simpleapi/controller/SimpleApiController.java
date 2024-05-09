0d38a5c2afbbaef69a7921a9f3938260b5c7835e
남궁성환 <saturn203@naver.com>
2024-05-09 18:14:11 +0900

    dsfsd

--- src/main/java/com/example/simpleapi/controller/SimpleApiController.java
+++ src/main/java/com/example/simpleapi/controller/SimpleApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/disp/svr/api")
 @Slf4j
 public class SimpleApiController {
 	@GetMapping("/hello")
@@ -40,6 +40,7 @@ public class SimpleApiController {
 	public String version(){
 		//log.info("version 1.0");
 		return "=====simple-api  version 2.0";
+
 		
 	}
 
