439a4f8daae39b0d42d17d940544f300edf7a809
남궁성환 <saturn203@naver.com>
2024-05-25 09:40:25 +0900

    dsfsd

--- src/main/java/com/example/simpleapi/controller/SimpleApiController.java
+++ src/main/java/com/example/simpleapi/controller/SimpleApiController.java
@@ -1,13 +1,16 @@
 package com.example.simpleapi.controller;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
-import com.example.simpleapi.model.Simple;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.simpleapi.model.Simple;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,13 +19,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SimpleApiController {
 	@GetMapping("/hello")
-	public String hello() {
+	public String hello(@RequestParam(required = false) String error, HttpServletRequest request) throws Exception {
+		
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			log.info("{} : {}", headerName, request.getHeader(headerName));
+		}
+		
+		if("true".equals(error)){
+			throw new Exception("Error: true");
+		}
 		
 		log.info("==========simple-api home()");
 		
 		return "hello world";
 		
 	}
+	
 	@GetMapping("/simple")
 	public List<Simple> listSimple(){
 		List<Simple> list = new ArrayList<>();
@@ -31,6 +45,10 @@ public class SimpleApiController {
 			list.add(new Simple(i+1,"test-"+i, "contents-"+i));
 			log.info("for "+i);
 		}
+		// for(int i=0 ; i< 10;i++) {
+		// 	list.add(new Simple(i+1,"test-"+i, "contents-"+i));
+		// 	log.info("for "+i);
+		// }
 		log.info(list.toString());
 		return list;
 		
